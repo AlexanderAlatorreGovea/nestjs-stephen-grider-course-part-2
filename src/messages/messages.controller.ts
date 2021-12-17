d79054a41b084555c814870b2b86469805a283b4
Alexander <Alexander_alatorre1993@hotmail.com>
2021-12-16 18:06:01 -0800

    added messages dto

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { createMessageDto } from './dtos/create-messages.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessages(@Body() body: createMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessages(@Param('id') id: string) {
+      console.log(id)
+  }
+}
